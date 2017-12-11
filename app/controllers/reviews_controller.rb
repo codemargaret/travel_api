f94dc5e90cb786a6be213b0a41c2fc2f86a40ef0
Margaret Berry and Sarah Lindmar <margaret@>
2017-12-11 10:54:57 -0800

    add exception to invalid object creation and object not found

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,14 +1,33 @@
 class ReviewsController < ApplicationController
-
+  
   def index
-    @reviews = {"reviews": "This product sucks, just like your mom did last night!"}
+    @reviews = Review.all
     json_response(@reviews)
+  end
+
+  def show
+    @review = Review.find(params[:id])
+    json_response(@review)
+  end
+
+  def create
+    @review = Review.create!(review_params)
+    json_response(@review)
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    @review.update(review_params)
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
   end
 
   private
 
-  def json_response(object)
-    render json: object, status: :ok
+  def review_params
+    params.permit(:content, :rating, :user_id, :destination_id)
   end
-
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :destination
   belongs_to :user
+  validates :user_id, :presence => true
+  validates :content, :presence => true
+  validates :rating, :presence => true
+  validates :destination_id, :presence => true
 end
